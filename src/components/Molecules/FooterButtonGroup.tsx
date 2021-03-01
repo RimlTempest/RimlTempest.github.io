dc39dc099cba21e3b634b2d70992229f1e914cf9
RimlTempest <riml.slime@gmail.com>
2021-03-01 18:06:33 +0900

    refactoring: リンクを定数に分離

--- src/components/Molecules/FooterButtonGroup.tsx
+++ src/components/Molecules/FooterButtonGroup.tsx
@@ -2,20 +2,18 @@ import * as React from 'react';
 import { Stack } from '@chakra-ui/react';
 import { FaFacebook, FaTwitter, FaGithub } from 'react-icons/fa';
 import { SocialButton } from '../Atoms/SocialButton';
+import { SNS } from '../../types/Sns';
 
-export const FooterButtonGroup = () => {
-  const twitter: string = 'https://twitter.com/Fande4d';
-  const github: string = 'https://github.com/RimlTempest';
-  const facebook: string = 'https://facebook.com/R1mlTempest';
+export const FooterButtonGroup: React.FC = () => {
   return (
     <Stack direction={'row'} spacing={6}>
-      <SocialButton label={'Twitter'} href={twitter}>
+      <SocialButton label={'Twitter'} href={SNS.TWITTER}>
         <FaTwitter />
       </SocialButton>
-      <SocialButton label={'Github'} href={github}>
+      <SocialButton label={'Github'} href={SNS.GITHUB}>
         <FaGithub />
       </SocialButton>
-      <SocialButton label={'Facebook'} href={facebook}>
+      <SocialButton label={'Facebook'} href={SNS.FACEBOOK}>
         <FaFacebook />
       </SocialButton>
     </Stack>
