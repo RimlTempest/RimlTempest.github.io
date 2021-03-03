7e82daf0168f2a0fdeb1b91818265eb9a8ada958
RimlTempest <riml.slime@gmail.com>
2021-03-03 21:08:57 +0900

    add: i18n対応につき言語ファイル、型定義ファイル、hookの追加

---
+++ locales/localeTypes.ts
@@ -0,0 +1,22 @@
+export type LocaleType = {
+    // 共有
+    FUTURE_RELEASE: string,
+    // Header
+    TOPPAGE_HEADER_TEXT: string,
+    ABOUT_HEADER_TEXT: string,
+    PRODUCTS_HEADER_TEXT: string,
+    CONTACT_HEADER_TEXT: string,
+    // About
+    ABOUT_TITLE: string,
+    // Products
+    PRODUCTS_TITLE: string,
+    // Product
+    PRODUCT_TITLE: string,
+    // Contact
+    CONTACT_TITLE: string,
+    FRIEND_CODE_COPY_MESSAGE: string,
+    ARCADE_GAME_CARD_TITLE: string,
+    MOBILE_GAME_CARD_TITLE: string,
+    CONTACT_CARD_TITLE: string,
+    SUB_CONTACT_CARD_TITLE: string
+}
