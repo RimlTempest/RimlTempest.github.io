a8b3cd902e2cf4eae46095e182de77fa398b6b8b
RimlTempest <riml.slime@gmail.com>
2021-03-05 22:24:46 +0900

    fix: カスタムフックの型を付与

--- src/hooks/useLocale.ts
+++ src/hooks/useLocale.ts
@@ -1,9 +1,10 @@
-import { useRouter } from 'next/router';
+import { NextRouter, useRouter } from 'next/router';
 import { ja } from '../../locales/ja';
 import { en } from '../../locales/en';
+import { LocaleType } from '../../locales/localeTypes';
 
 export const useLocale = () => {
-    const { locale } = useRouter();
-    const i18n = ja; // locale === "ja" ? ja : en;
+    const { locale }: NextRouter = useRouter();
+    const i18n: LocaleType = ja; // locale === "ja" ? ja : en;
     return { locale, i18n }
 }
