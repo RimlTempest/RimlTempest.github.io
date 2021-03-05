5b02d0b0361ab823c86971266350742ff847fcdc
RimlTempest <riml.slime@gmail.com>
2021-03-06 06:32:25 +0900

    fix: 余計なimportを削除

--- src/components/Molecules/SkillGroup.tsx
+++ src/components/Molecules/SkillGroup.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Flex, Td, Text, Tr } from '@chakra-ui/react';
+import { Td, Tr } from '@chakra-ui/react';
 import { SkillProgress } from '../../components/Atoms/SkillProgress';
-import { CopyIcon } from '@chakra-ui/icons';
 import { SkillGroupProps } from '../../types/SkillGroupTypes';
 
 export const SkillGroup: React.FC<SkillGroupProps> = (
